3c3c2b9410e4bb0d00455439e9e6b5bd72589824
hokuto.hirano <aaa@bbb.com>
2024-04-03 12:40:48 +0900

    update

---
+++ app/image-viewer.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import React, { useState } from 'react';
+
+export default function ImageViewer() {
+    const [imageData, setImageData] = useState<string | null>(null);
+
+    const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const file = event.target.files?.[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                const dataUrl = reader.result as string;
+                setImageData(dataUrl);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    return (
+        <div>
+            <input type="file" accept="image/*" onChange={handleImageUpload} />
+            {imageData && <img src={imageData} alt="Selected Image" />}
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import Greet from "./greet";
+import ImageViewer from "./image-viewer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Greet />
+
+      <ImageViewer />
     </main>
   );
 }
