69dd38d15f4bb16cf483f9cbf788f7c2c3d706e6
hokuto.hirano <aaa@bbb.com>
2024-04-07 12:01:38 +0900

    update

--- main/app/components/common/Header/index.tsx
+++ main/app/components/common/Header/index.tsx
@@ -1,20 +1,46 @@
 import styles from './Header.module.css';
 
-import { Button, IconButton } from "@mui/material";
+import { Button, IconButton, Menu, MenuItem } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
+import { useState } from 'react';
 
 export default function Header() {
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <div className={styles.container}>
-            <span className={styles.title}>Cellgram</span>
-
             <div>
-                <IconButton aria-label="delete" size="large">
+                <span className={styles.title}>Cellgram</span>
+
+                <IconButton aria-label="delete" size="large" aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClick}>
                     <MenuIcon />
                 </IconButton>
 
-                <Button variant="contained">Test</Button>
+                <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                        'aria-labelledby': 'basic-button',
+                    }}
+                >
+                    <MenuItem onClick={handleClose}>Open</MenuItem>
+                </Menu>
             </div>
+
+            <Button variant="contained">Test</Button>
         </div>
     );
 }
